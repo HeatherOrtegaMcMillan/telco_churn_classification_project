6910fcaaf14d78cd587c9cc8d3e7b2f422a79b32
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-05-25 23:56:04 -0500

    add prepare.py

---
+++ prepare.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import acquire
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer 
+
+
+def prep_telco(df):
+    """
+    This functions takes in the telco churn dataframe and retuns the cleaned and prepped dataset
+    """
+    df.total_charges = df.total_charges.str.replace(' ', '0').astype(float)
+
+def test_train_split(df):
+    '''
+    args: df
+    This function take in the telco_churn data data acquired by aquire.py, get_db_data(),
+    performs a split, stratifies by churn.
+    Returns train, validate, and test dfs.
+    '''
+     train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=713,
+                                        stratify = df.churn)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=713,
+                                   stratify= train.churn)
+    return train, validate, test
